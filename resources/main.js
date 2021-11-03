be3ce18c6aed54aac403f2f6a8722e5ed427cedf
Gage Miller <miller2590@gmail.com>
2021-11-03 11:12:55 -0500

    removed position variable

--- resources/main.js
+++ resources/main.js
@@ -2,7 +2,6 @@ let game_board = document.body.getElementsByClassName('game-board')[0];
 let squares = game_board.getElementsByTagName('div');
 
 let player = 1;
-let position = 0;
 
 function change_player() {
     if (player === 1) {
