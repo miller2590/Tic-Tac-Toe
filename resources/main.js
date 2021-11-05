f7f6d5d4579933d3737e9dbfc6449586e11384c3
Gage Miller <miller2590@gmail.com>
2021-11-05 18:36:07 -0500

    added notes

--- resources/main.js
+++ resources/main.js
@@ -1,25 +1,29 @@
-let game_board = document.body.getElementsByClassName('game-board')[0];
-let squares = game_board.getElementsByTagName('div');
+let game_board = document.body.getElementsByClassName("game-board")[0];
+let squares = game_board.getElementsByTagName("div");
 
 let player = 1;
 
 function change_player() {
-    if (player === 1) {
-        player ++;
-
-    } else {
-        player --;
-    }
-};
-
+  if (player === 1) {
+    player++;
+  } else {
+    player--;
+  }
+}
 
 // Checks if square is available, if so, it places an X or O
 for (let i = 0; i < 9; i++) {
-    squares[i].addEventListener('click', () => {
-        if (squares[i].innerHTML === 'O' || squares[i].innerHTML === 'X') {
-            alert('Invalid move')
-        } else {
-            squares[i].innerHTML = 'X'
-        }
-    })
+  squares[i].addEventListener("click", () => {
+    if (squares[i].innerHTML === "O" || squares[i].innerHTML === "X") {
+      alert("Invalid move");
+    } else {
+      squares[i].innerHTML = "X";
+    }
+  });
 }
+
+function isWinner() {
+  //
+}
+
+// Need to differentiate between players and X's and O's
