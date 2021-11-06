f44a325d09fcb725672f46f565dcc6447015fa5e
miller2590 <miller2590@gmail.com>
2021-11-06 17:23:39 -0500

    need to find a work around for eventListener

--- resources/main.js
+++ resources/main.js
@@ -2,6 +2,24 @@ let game_board = document.body.getElementsByClassName("game-board")[0];
 let squares = game_board.getElementsByTagName("div");
 
 let player = 1;
+let isWinner = false;
+
+function validMove() {
+  // Checks if square is available, and player status and it places an X or O
+  for (let i = 0; i < 9; i++) {
+    squares[i].addEventListener("click", () => {
+      if (squares[i].innerHTML === "O" || squares[i].innerHTML === "X") {
+        alert("Invalid move");
+      } else if (player === 1) {
+        squares[i].innerHTML = 'X';
+        change_player()
+      } else if (player === 2) {
+        squares[i].innerHTML = 'O';
+        change_player()
+      }
+    });
+  }
+}
 
 function change_player() {
   if (player === 1) {
@@ -11,24 +29,25 @@ function change_player() {
   }
 }
 
-// Checks if square is available, if so, it places an X or O
-for (let i = 0; i < 9; i++) {
-  squares[i].addEventListener("click", () => {
-    if (squares[i].innerHTML === "O" || squares[i].innerHTML === "X") {
-      alert("Invalid move");
-    } else if (player === 1) {
-      squares[i].innerHTML = 'X';
-      change_player()
-    } else if (player === 2) {
-      squares[i].innerHTML = 'O';
-      change_player()
-    }
-  });
+function boardIsFull() {
+  let isFull = false;
+  let checkSquares = Object.values(squares).every(value => value.innerHTML != "");
+  if (checkSquares == true) {
+    isFull = true;
+  }
+  return isFull;
+}
+
+function endGame() {
+  if (boardIsFull()) {
+    alert('board is full')
+  }
 }
 
 
-function isWinner() {
-  //
-}
+
+
+validMove()
+endGame()
 
 // Need to differentiate between players and X's and O's
