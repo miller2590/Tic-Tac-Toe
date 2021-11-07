222b5479b447eb1b60813ef807e37c8570822108
miller2590 <59415200+miller2590@users.noreply.github.com>
2021-11-06 20:05:10 -0500

    fixed eventListener issue

--- resources/main.js
+++ resources/main.js
@@ -4,20 +4,37 @@ let squares = game_board.getElementsByTagName("div");
 let player = 1;
 let isWinner = false;
 
-function validMove() {
-  // Checks if square is available, and player status and it places an X or O
-  for (let i = 0; i < 9; i++) {
-    squares[i].addEventListener("click", () => {
-      if (squares[i].innerHTML === "O" || squares[i].innerHTML === "X") {
-        alert("Invalid move");
-      } else if (player === 1) {
-        squares[i].innerHTML = 'X';
-        change_player()
-      } else if (player === 2) {
-        squares[i].innerHTML = 'O';
-        change_player()
-      }
-    });
+
+// function validMove() {
+//   // Checks if square is available, and player status and it places an X or O
+//   for (let i = 0; i < 9; i++) {
+//     squares[i].addEventListener("click", () => {
+//       if (squares[i].innerHTML === "O" || squares[i].innerHTML === "X") {
+//         alert("Invalid move");
+//       } else if (player === 1) {
+//         squares[i].innerHTML = 'X';
+//         change_player()
+//       } else if (player === 2) {
+//         squares[i].innerHTML = 'O';
+//         change_player()
+//       }
+//     });
+//   }
+// }
+
+
+function squareClick(clickedSquare) {
+
+  const currentSquare = clickedSquare.target;
+
+  if (currentSquare.innerHTML === "O" || currentSquare.innerHTML === "X") {
+    alert("Invalid move");
+  } else if (player === 1) {
+    currentSquare.innerHTML = 'X';
+    change_player()
+  } else if (player === 2) {
+    currentSquare.innerHTML = 'O';
+    change_player()
   }
 }
 
@@ -45,9 +62,4 @@ function endGame() {
 }
 
 
-
-
-validMove()
-endGame()
-
-// Need to differentiate between players and X's and O's
+document.querySelectorAll('.square').forEach(square => square.addEventListener('click', squareClick));
