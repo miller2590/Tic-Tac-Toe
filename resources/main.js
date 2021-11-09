74f946d4c721c15037f12107e779050ee5d45d81
miller2590 <miller2590@gmail.com>
2021-11-08 20:48:02 -0600

    Created makeshift restart button

--- resources/main.js
+++ resources/main.js
@@ -1,4 +1,5 @@
-let squares = document.querySelectorAll(".square");
+let squares = document.querySelectorAll('.square');
+let restart = document.querySelector('.restart-btn');
 
 let player = 1;
 
@@ -99,4 +100,11 @@ function endGame() {
   }
 }
 
+function refresh() {
+  let grid = Object.values(squares);
+  
+  grid.forEach(item => item.innerHTML = "")
+}
+
 squares.forEach((square) => square.addEventListener("click", squareClick));
+restart.addEventListener('click', refresh);
